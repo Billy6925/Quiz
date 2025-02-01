899f9f0a8ef989e389098084036cbc6f456ddcc5
Natasha Nduku <natashanduku0@gmail.com>
2025-02-01 15:10:37 +0300

    state management

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Result() {
+  const [questions, setQuestions] = useState([]);
+  const [results, setResults] = useState([]);
+  const [score, setScore] = useState(0);
   return <div>Result Component</div>;
 }
 
