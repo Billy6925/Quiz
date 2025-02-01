f9b35ee28d9fdd0ed8d997b32008ef8e4ec00207
Natasha Nduku <natashanduku0@gmail.com>
2025-02-01 15:14:06 +0300

    adds to local storage

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -5,6 +5,19 @@ function Result() {
   const [questions, setQuestions] = useState([]);
   const [results, setResults] = useState([]);
   const [score, setScore] = useState(0);
+  
+  useEffect(() => {
+    const storedQuiz = JSON.parse(localStorage.getItem("lastQuizData")) || {};
+    const { questions = [], results = [] } = storedQuiz;
+  
+    setQuestions(questions);
+    setResults(results);
+  
+    const correctCount = results.filter(r => r.isCorrect).length;
+    setScore(correctCount);
+  }, []);
+  
+
   return <div>Result Component</div>;
 }
 
